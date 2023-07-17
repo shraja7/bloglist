4cb27fb2b3207b063f126dd9dcef5e307e2f86d3
saad raja <shraja7@gmail.com>
2023-07-17 12:08:43 -0500

    successfully get and post data to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,40 @@
-//require express and cors
+require("dotenv").config();
+const connectToDatabase = require("./app");
 const express = require("express");
 const cors = require("cors");
 const Blog = require("./models/blog");
-const connectToDatabase = require("./app");
 
 const app = express();
+const port = process.env.PORT || 3003;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+app.get("/api/blogs", async (req, res) => {
+  try {
+    const blogs = await Blog.find({});
+    res.json(blogs);
+  } catch (error) {
+    res.status(500).json({ error: "Error retrieving blogs from the database" });
+  }
 });
 
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+app.post("/api/blogs", async (req, res) => {
+  try {
+    const blog = new Blog(req.body);
+    const savedBlog = await blog.save();
+    res.status(201).json(savedBlog);
+  } catch (error) {
+    res.status(500).json({ error: "Error saving the blog to the database" });
+  }
 });
 
 connectToDatabase()
   .then(() => {
-    const PORT = 3003;
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
     });
   })
   .catch((error) => {
     console.log("Error connecting to database:", error);
   });
-
-// const PORT = 3003;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
